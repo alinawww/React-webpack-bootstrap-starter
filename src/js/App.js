d01756ec6d406d683c7cd85787b1ce4927eccddc
Alina Tudose <alina.tudose@travelbird.com>
2017-08-12 13:30:25 +0200

    make collapsible component

--- src/js/App.js
+++ src/js/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import image from '../images/expand-vertical-4.svg';
+import Collapsible from './collapsible'
 
 class App extends React.Component {
     render() {
@@ -11,36 +12,15 @@ class App extends React.Component {
                 </header>
                 <div className="content">
                     <div className="panel-group">
-                        <div className="panel">
-                            <div className="panel-heading">
-                                <h2>Overview</h2>
-                            </div>
-                            <div className="panel-collapse">
-                                <div className="panel-body">
-                                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute.
-                                </div>
-                            </div>
-                        </div>
-                        <div className="panel">
-                            <div className="panel-heading">
-                                <h2>Features</h2>
-                            </div>
-                            <div className="panel-collapse">
-                                <div className="panel-body">
-                                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute.
-                                </div>
-                            </div>
-                        </div>
-                        <div className="panel">
-                            <div className="panel-heading">
-                                <h2>Reviews</h2>
-                            </div>
-                            <div className="panel-collapse">
-                                <div className="panel-body">
-                                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute.
-                                </div>
-                            </div>
-                        </div>
+                        <Collapsible title="Overview">
+                          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                        </Collapsible>
+                        <Collapsible title="Features">
+                          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </Collapsible>
+                        <Collapsible title="Software">
+                          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </Collapsible>
                     </div>
                 </div>
             </div>
